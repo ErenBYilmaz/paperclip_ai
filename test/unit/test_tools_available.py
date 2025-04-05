d2d203f3b43cbb88a15b6e7040940839805d45de
Eren Yilmaz <erenyilmaz95@gmx.de>
2025-04-05 23:56:53 +0200

    Fix playwright installation

--- test/unit/test_tools_available.py
+++ test/unit/test_tools_available.py
@@ -11,7 +11,7 @@ class TestTools(unittest.TestCase):
         tools = a.tools()
         assert any(tool.name.lower().startswith('playwright') for tool in tools), "No playwright tools available"
 
-    def test_navigate(self):
+    def test_navigate_and_save_pdf(self):
         print()
         print(os.environ['DISPLAY'])
         assert os.environ['DISPLAY'] == 'host.docker.internal:0.0'
@@ -24,7 +24,33 @@ class TestTools(unittest.TestCase):
 
         async def call():
             async with server:
-                await server.call_tool('playwright_navigate', {"url": "https://www.example.com"})
+                print(await server.call_tool('playwright_navigate', {"url": "https://www.example.com", "browserType": 'chromium'}))
+                print(await server.call_tool('playwright_save_as_pdf', {"outputPath": "/code/", "filename": "test.pdf", "browserType": 'chromium'}))
+                print('Waiting 10 seconds for the server to process the request...')
+                await asyncio.sleep(10)
+            await asyncio.sleep(1)
+
+        asyncio.run(call())
+
+    def test_if_cookies_are_being_stored(self):
+        print()
+        print(os.environ['DISPLAY'])
+        assert os.environ['DISPLAY'] == 'host.docker.internal:0.0'
+        a = Agent()
+        servers = a.mcp_server_stack.mcp_servers
+        print([server.name for server in servers])
+        servers = [server for server in servers if server.name == "playwright"]
+        assert len(servers) == 1
+        server = servers[0]
+
+        async def call():
+            async with server:
+                print(await server.call_tool('playwright_navigate', {"url": "https://www.decisionproblem.com/paperclips/index2.html", "browserType": 'chromium'}))
+                print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
+                print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
+                print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
+                print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
+                print('Waiting 10 seconds for the server to process the request...')
                 await asyncio.sleep(10)
             await asyncio.sleep(1)
 
