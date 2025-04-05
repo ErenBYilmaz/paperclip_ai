a30dbb4ba12645927f0c1232c9df879d9904460c
Eren Yilmaz <erenyilmaz95@gmx.de>
2025-04-06 00:34:39 +0200

    Test loading save games

--- test/unit/test_tools_available.py
+++ test/unit/test_tools_available.py
@@ -3,6 +3,7 @@ import os
 import unittest
 
 from agent.paperclip_agent import Agent
+from test.resources import example_savegame_8_clips
 
 
 class TestTools(unittest.TestCase):
@@ -46,10 +47,19 @@ class TestTools(unittest.TestCase):
         async def call():
             async with server:
                 print(await server.call_tool('playwright_navigate', {"url": "https://www.decisionproblem.com/paperclips/index2.html", "browserType": 'chromium'}))
+                json_dumped_save = example_savegame_8_clips
+                javascript_call_for_restoring_savegame = f's = JSON.parse({json_dumped_save});' + 'for (const key in s){localStorage.setItem(key, s[key])};load()'
+                print(javascript_call_for_restoring_savegame)
+                print(await server.call_tool('playwright_evaluate', {"script": javascript_call_for_restoring_savegame}))
                 print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
                 print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
                 print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
                 print(await server.call_tool('playwright_click', {"selector": "#btnMakePaperclip"}))
+                print(await server.call_tool('playwright_evaluate', {"script": "save();JSON.stringify(localStorage)"}))
+                html_response = await server.call_tool('playwright_get_visible_html', {})
+                print(html_response)
+                html_response = '\n'.join(html_response.content[0].text.splitlines()[1:])
+                print(html_response)
                 print('Waiting 10 seconds for the server to process the request...')
                 await asyncio.sleep(10)
             await asyncio.sleep(1)
