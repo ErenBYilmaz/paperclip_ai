3c79c09027c7be64fff61663dbd61aeaf11bb3ed
Eren Yilmaz <erenyilmaz95@gmx.de>
2025-04-05 23:31:28 +0200

    Get playwright running inside docker

--- agent/playwright_mcp_with_ui.sh
+++ agent/playwright_mcp_with_ui.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env -S bash -x
+#!/usr/bin/env -S bash -ex
 export DISPLAY=host.docker.internal:0.0
 Xvfb host.docker.internal:0.0 -screen 0 1280x800x24 -ac >/dev/null 2>&1 &
 x11vnc -display host.docker.internal:0.0 -forever -rfbauth /home/myuser/.vncpass -listen 0.0.0.0 -rfbport 5900 >/dev/null 2>&1 &
 sudo service dbus start
 echo $DISPLAY
 sleep 1
-npx -y @modelcontextprotocol/server-puppeteer 3<&0 2>&1
+#npx --no-install @modelcontextprotocol/server-puppeteer 3<&0 2>&1
+npx --no-install @executeautomation/playwright-mcp-server 3<&0 2>&1
--- test/unit/test_tools_available.py
+++ test/unit/test_tools_available.py
@@ -6,10 +6,10 @@ from agent.paperclip_agent import Agent
 
 
 class TestTools(unittest.TestCase):
-    def test_puppeteer_tools_available(self):
+    def test_playwright_tools_available(self):
         a = Agent()
         tools = a.tools()
-        assert any(tool.name.startswith('puppeteer') for tool in tools), "No puppeteer tools available"
+        assert any(tool.name.lower().startswith('playwright') for tool in tools), "No playwright tools available"
 
     def test_navigate(self):
         print()
@@ -18,13 +18,13 @@ class TestTools(unittest.TestCase):
         a = Agent()
         servers = a.mcp_server_stack.mcp_servers
         print([server.name for server in servers])
-        servers = [server for server in servers if server.name == "puppeteer"]
+        servers = [server for server in servers if server.name == "playwright"]
         assert len(servers) == 1
         server = servers[0]
 
         async def call():
             async with server:
-                await server.call_tool('puppeteer_navigate', {"url": "https://www.example.com"})
+                await server.call_tool('playwright_navigate', {"url": "https://www.example.com"})
                 await asyncio.sleep(10)
             await asyncio.sleep(1)
 
